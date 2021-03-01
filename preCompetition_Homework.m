5c1463469af0c1a4864d3a503a1c3951ab1ee375
Zoe Despature <zoe.despature@gmail.com>
2021-03-01 11:49:11 +0000

    Adding psth to git

--- preCompetition_Homework.m
+++ preCompetition_Homework.m
@@ -54,9 +54,13 @@ for iTrial = 1:length(trial)    % For loop which goes from the first trial of a 
 end
 
 figure;
-nbins               = 20;       % divide range of values into nbins intervals, count how many values fall into each interval
+nbins               = 3;       % divide range of values into nbins intervals, count how many values fall into each interval
 h                   = histogram(spks,nbins);    % create histogram
 h.FaceColor         = 'k';                      % change color of histogram to black
+
+% [N,edges] = histcounts(spks, 'Normalization','pdf');
+% edges = edges(2:end) - (edges(2)-edges(1))/2;
+% plot(edges, N);
 
 title('Post Stimulus Time Histogram (PTSH)');
 xlabel('Time (ms)');
@@ -147,3 +151,37 @@ for i = 1:8
 end
 
 legend('Movement 1', 'Movement 2', 'Movement 3', 'Movement 4', 'Movement 5', 'Movement 6', 'Movement 7', 'Movement 8');
+
+
+%% Histogram for each neural unit
+
+
+figure;
+
+for iNeuralUnit = 1:size(trial(iTrial,2).spikes,1)
+
+    spks = [];      % Spks array is created outside of for loop to gather all the time stamps at which neural unit 1 fired through every trial
+
+    for iTrial = 1:length(trial)    % For loop which goes from the first trial of a movement til the last one (1 to 100)
+
+        for i = 1:length(trial(iTrial,2).spikes)        % For loop to go through the array of the first neural unit of each trial
+            if trial(iTrial,2).spikes(iNeuralUnit,i) == 1         % If loop which detects when the unit fired (when its =1)
+                spks = [spks i];                        % Add the time stamp at which the unit fired to the spks array
+            end
+        end
+
+    end
+
+    subplot(98,1,iNeuralUnit);
+
+    nbins               = 3;       % divide range of values into nbins intervals, count how many values fall into each interval
+    h                   = histogram(spks,nbins);    % create histogram
+    h.FaceColor         = 'k';                      % change color of histogram to black
+    xlim([0, 1000]);
+
+end
+
+
+% title('Post Stimulus Time Histogram (PTSH)');
+% xlabel('Time (ms)');
+% ylabel('spike count');
