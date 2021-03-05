6941c7bb1b2757a735121eecae2102df5afc3125
simonoury <simonoury@simons-mbp.lan>
2021-03-05 11:49:56 +0000

    K-nearest neighboors model train and test

--- KNN.m
+++ KNN.m
@@ -1,17 +1,18 @@
 close all;
+clear all;
 
 load('monkeydata_training.mat')
 
 
-%%
+%% This code creates the dataset
 X = zeros(800, 98)
 Y = zeros(800, 1)
 
 trial_id = 0
 Y = zeros(800, 1)
-fire_rates = zeros(800, 98)
-for movement = 1:8
-    for trial_num = 1:100
+X = zeros(800, 98)
+for trial_num = 1:100    
+    for movement = 1:8
         trial_id = trial_id + 1;
         for neuron = 1:98
             count = 0;
@@ -20,8 +21,29 @@ for movement = 1:8
                     count = count + 1;
                 end
             end
-            fire_rates(trial_id, neuron) = count;
+            X(trial_id, neuron) = count;
             Y(trial_id, 1) = movement;
         end
     end
 end
+
+%%
+train_X = X(1:700,:)
+train_Y = Y(1:700,:)
+
+test_X = X(701:800, :)
+test_Y = Y(701:800, :)
+
+
+%%
+model = fitcknn(train_X,train_Y)
+
+%%
+true = 0
+for i = 1:100
+    label = predict(model, test_X(i, :))
+    if label == test_Y(i)
+        true = true + 1
+    end
+end
+accuracy = true/100
