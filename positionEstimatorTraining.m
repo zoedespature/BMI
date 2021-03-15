bb1e49222375f8c93b0b80872ae43186fc8b423c
Zoe Despature <zoe.despature@gmail.com>
2021-03-15 13:05:36 +0000

    Updating Position Estimator Training function with means method

--- positionEstimatorTraining.m
+++ positionEstimatorTraining.m
@@ -1,37 +1,113 @@
-<<<<<<< HEAD
-function [outputArg1,outputArg2] = positionEstimatorTraining(inputArg1,inputArg2)
-%POSITIONESTIMATORTRAINING Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function [modelParameters] = positionEstimatorTraining(trainingData)
+  % Arguments:
+  
+  % - training_data:
+  %     training_data(n,k)              (n = trial id,  k = reaching angle)
+  %     training_data(n,k).trialId      unique number of the trial
+  %     training_data(n,k).spikes(i,t)  (i = neuron id, t = time)
+  %     training_data(n,k).handPos(d,t) (d = dimension [1-3], t = time)
+  
+  % ... train your model
+  
+  % Return Value:
+  
+  % - modelParameters:
+  %     single structure containing all the learned parameters of your
+  %     model and which can be used by the "positionEstimator" function.
+  
+  
+modelParameters = struct();
 
-=======
-function model = positionEstimatorTraining(trial)
 
-X = zeros(720, 98);
-Y = zeros(720, 1);
+% KNN
 
 trial_id = 0;
-Y = zeros(720, 1);
-X = zeros(720, 98);
-for trial_num = 1:100    
-    for movement = 1:8
+Y = zeros(length(trainingData)*size(trainingData,2), 1);
+X = zeros(length(trainingData)*size(trainingData,2), 98);
+
+for trial_num = 1:length(trainingData)   
+    
+    for movement = 1:size(trainingData,2)
+        
         trial_id = trial_id + 1;
-        for neuron = 1:98
+        
+        for neuron = 1:size(trainingData(trial_num, movement).spikes,1)
             count = 0;
+            
             for i = 1:300
-                if trial(trial_num, movement).spikes(neuron, i) == 1
+                if trainingData(trial_num, movement).spikes(neuron, i) == 1
                     count = count + 1;
                 end
             end
+            
             X(trial_id, neuron) = count;
             Y(trial_id, 1) = movement;
+            
         end
     end
 end
 
-model = fitcknn(train_X,train_Y);
+modelParameters.knn = fitcknn(X,Y);
 
+
+% Calculating mean
+
+mean_movements_x = zeros(8,1000);
+
+for iMovement = 1:size(trainingData,2)
+
+    mean_trial = zeros(length(trainingData),1000);
+
+        for iTrial = 1:length(trainingData)
+
+            for iTime = 1:size(trainingData(iTrial,iMovement).handPos)
+
+            mean_trial(iTrial,:) = [trainingData(iTrial,iMovement).handPos(1,:) zeros(1,1000-length(trainingData(iTrial,iMovement).handPos))];
+            
+
+            end
+
+        end
+        
+    mean_trial(mean_trial == 0) = NaN;
+
+    mean_trial = mean(mean_trial, 'omitnan');
+
+    mean_trial = mean_trial(1, 1:1000);
+    
+    mean_movements_x(iMovement,:) = mean_trial;
+    
 end
->>>>>>> b49e4ca7c2f6efdca82831d1dac36f0b10b8d45f
+
+mean_movements_y = zeros(8,1000);
+
+for iMovement = 1:size(trainingData,2)
+
+    mean_trial = zeros(length(trainingData),1000);
+
+        for iTrial = 1:length(trainingData)
+
+            for iTime = 1:size(trainingData(iTrial,iMovement).handPos)
+
+            mean_trial(iTrial,:) = [trainingData(iTrial,iMovement).handPos(2,:) zeros(1,1000-length(trainingData(iTrial,iMovement).handPos))];
+            
+            
+            end
+
+        end
+        
+    mean_trial(mean_trial == 0) = NaN;
+    
+    mean_trial = mean(mean_trial, 'omitnan');
+
+    mean_trial = mean_trial(1, 1:1000);
+    
+    mean_movements_y(iMovement,:) = mean_trial;
+    
+end
+
+modelParameters.mean_movements_x = mean_movements_x;
+modelParameters.mean_movements_y = mean_movements_y;
+
+  
+end
