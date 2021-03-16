349172d6ac1bf168dc897142d36fbbc71ede0dd7
simonoury <simonoury@simons-mbp.lan>
2021-03-16 09:58:49 +0000

    add to folder

---
+++ Kalman/Kalman_train.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Kalman(inputArg1,inputArg2)
+%KALMAN Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
